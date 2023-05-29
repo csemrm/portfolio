9b50f66ad7436d7b8f6abf2966eedb3cb38f27a6
Mostafizur Rahman <csemrm@gmail.com>
2023-05-28 23:07:04 -0700

    Routing

--- home/views.py
+++ home/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render, HttpResponse
 
 
 def index(request):
-    return HttpResponse("hello")
+    return HttpResponse("This is Index")
+
+def about(request):
+    return HttpResponse("This is about")
+
+def projects(request):
+    return HttpResponse("This is my projects")
+
+def contact(request):
+    return HttpResponse("This is contact")
